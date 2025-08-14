d64a19b5d56534e2bee585287f66e95d474e9e74
luoliwoshang <2643523683@qq.com>
2025-08-14 18:03:13 +0800

    error case

--- independant-llgo-esp/main.go
+++ independant-llgo-esp/main.go
@@ -9,19 +9,20 @@ import (
 )
 
 func main() {
+	// "github.com/luoliwoshang/llgo-esp32/emb/machine" will cause memory error
+	// _ = machine.GPIO34
 	tag := c.Str("llgo-esp32")
 	time := os.TickTypeT(c.Uint32T(10))
 	btnIO := gpio.GpioNumT(gpio.GPIO_NUM_34)
 	gpio.GpioSetDirection(btnIO, gpio.GPIO_MODE_INPUT)
-	log.EspLogWrite(log.ESP_LOG_INFO, tag, c.Str("hello llgo-esp32!!!!!\n"))
+	log.EspLogWrite(log.ESP_LOG_INFO, tag, c.Str("hello～～～～ llgo-esp32!!!!!\n"))
 	for {
 		level := gpio.GpioGetLevel(btnIO)
 		if level == 1 {
-			log.EspLogWrite(log.ESP_LOG_INFO, tag, c.Str("没按下去!!!!\n"))
+			log.EspLogWrite(log.ESP_LOG_INFO, tag, c.Str("没按下去dasdsadsadas～～～!!!!\n"))
 		} else {
-			log.EspLogWrite(log.ESP_LOG_INFO, tag, c.Str("按下去了!!!!\n"))
+			log.EspLogWrite(log.ESP_LOG_INFO, tag, c.Str("按下去了dasdasdas～～～～!!!!\n"))
 		}
 		time.VTaskDelay()
 	}
-
 }
