5a6073e3b545c49a5c42ad35db4d9a8447e65fd0
luoliwoshang <2643523683@qq.com>
2025-08-14 18:00:32 +0800

    modify start.sh

--- independant-llgo-esp/start.sh
+++ independant-llgo-esp/start.sh
@@ -1,8 +1,8 @@
-export PATH="/Users/zhangzhiyang/.espressif/tools/esp-clang/esp-18.1.2_20240912/esp-clang/bin:$PATH"
-llgo build -o test main.go
+# export PATH="/Users/zhangzhiyang/Documents/Code/temp/tinygo/llvm-build/bin:$PATH"
+ export PATH="/Users/zhangzhiyang/.espressif/tools/esp-clang/esp-18.1.2_20240912/esp-clang/bin:$PATH" && llgo build -target esp32-coreboard-v2 -tags nogc -o test /Users/zhangzhiyang/Documents/Code/goplus/llgo-esp32/independant-llgo-esp/main.go
 # Convert ELF to BIN
 python /Users/zhangzhiyang/esp/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 elf2image --flash_mode dio --flash_freq 40m --flash_size 2MB --elf-sha256-offset 0xb0 --min-rev-full 0 --max-rev-full 399 -o /Users/zhangzhiyang/Documents/Code/goplus/llgo-esp32/independant-llgo-esp/test.bin /Users/zhangzhiyang/Documents/Code/goplus/llgo-esp32/independant-llgo-esp/test.elf
 # Flash the BIN to ESP32
 python -m esptool --chip esp32 -b 460800 --before default_reset --after hard_reset write_flash 0x10000 /Users/zhangzhiyang/Documents/Code/goplus/llgo-esp32/independant-llgo-esp/test.bin
 # Monitor the ESP32 output
-python /Users/zhangzhiyang/esp/esp-idf/tools/idf_monitor.py -p /dev/tty.usbserial-10 -b 115200 --toolchain-prefix "xtensa-esp32-elf-" --target esp32 --revision 0 /Users/zhangzhiyang/Documents/Code/goplus/llgo-esp32/independant-llgo-esp/test.elf
+python /Users/zhangzhiyang/esp/esp-idf/tools/idf_monitor.py -p /dev/tty.usbserial-110 -b 115200 --toolchain-prefix "xtensa-esp32-elf-" --target esp32 --revision 0 /Users/zhangzhiyang/Documents/Code/goplus/llgo-esp32/independant-llgo-esp/test.elf
